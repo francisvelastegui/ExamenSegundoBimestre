3228f88d77f6f511404bd839c36a7cc031c659cd
alexc492 <94476118+alexc492@users.noreply.github.com>
2023-03-12 16:33:05 -0500

    Correcciones1

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
+import GUI.Login;
+
 // NOMBRES: Francis Velastegui
 //          Carlos Villarreal
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Login login = new Login();
+        login.setVisible(true);
     }
 }
